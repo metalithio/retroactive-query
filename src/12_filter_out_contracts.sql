33c0fd5af0b7bfa0163869e76bddc11d1ec67f11
Mark Le <markle@Marks-MacBook-Pro.local>
2021-05-07 12:57:46 +0700

    get users by cohort

--- src/12_filter_out_contracts.sql
+++ src/12_filter_out_contracts.sql
@@ -1,3 +1,49 @@
 SELECT * FROM `bigquery-public-data.crypto_ethereum.contracts` 
 WHERE address = lower('0x8ae09173e4dff3f63d1c287d19f4a197977cb425')
 LIMIT 1000
+
+# get users by cohorts
+WITH
+all_contributors as (
+    select distinct fromAddress as address from `niftex-retroactive-query.retroactive_6b838dd00210ba679d4e172b01e573d784dacfac.v2_mainnet_contributors`
+    union distinct
+    select distinct toAddress as address from `niftex-retroactive-query.retroactive_6b838dd00210ba679d4e172b01e573d784dacfac.v2_mainnet_contributors`
+    union distinct
+    select distinct contributor as address from `niftex-retroactive-query.retroactive_6b838dd00210ba679d4e172b01e573d784dacfac.v1_contributors`
+),
+tier_one_holders as (
+    select distinct address from `niftex-retroactive-query.retroactive_6b838dd00210ba679d4e172b01e573d784dacfac.tier_one_mainnet_users`
+    union distinct (
+        with tier_one_matic_users as (
+            select address from `niftex-retroactive-query.retroactive_6b838dd00210ba679d4e172b01e573d784dacfac.tier_one_matic_users` limit 1
+        )
+        select distinct address from tier_one_matic_users cross join unnest(tier_one_matic_users.address) as address
+    )   
+),
+all_tier_users as (
+    select distinct address from `niftex-retroactive-query.retroactive_6b838dd00210ba679d4e172b01e573d784dacfac.all_tier_mainnet_users`
+    union distinct (
+        with all_tier_matic_users as (
+            select address from `niftex-retroactive-query.retroactive_6b838dd00210ba679d4e172b01e573d784dacfac.all_tier_matic_users` limit 1
+        )
+        select distinct address from all_tier_matic_users cross join unnest(all_tier_matic_users.address) as address
+    )
+    union distinct
+    select distinct address from all_contributors 
+),
+tier_one_users as (
+    select address, cohort from (
+        select address, 'tier 1' as cohort from (
+            select distinct address from all_contributors
+            union distinct
+            select distinct address from tier_one_holders
+        )
+    )
+),
+tier_three_users as (
+    select distinct address, 'tier 3' as cohort from all_tier_users
+    where address not in (
+        select distinct address from tier_one_users
+    )
+)
+select distinct address from all_tier_users;
